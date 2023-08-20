64fe2a582cb231a7b53d3c3cd4af3d629eb58a13
AldiiX <aldiix@email.cz>
2023-08-20 15:28:44 +0200

    1.1.20

---
+++ src/main/java/cz/aldiix/sessionsplugin/Sessions.java
@@ -0,0 +1,16 @@
+package cz.aldiix.sessionsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sessions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Sessions enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
